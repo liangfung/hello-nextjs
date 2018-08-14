38ba17dffa97cf7c26a6c1ab8b75595a6e16ebc0
fjl冯觉良 <jl_feng@ctrip.com>
2018-08-14 17:06:01 +0800

    init project and create layout

---
+++ .next/4.f36064d8771cf8d0b06b.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate(4,{
+
+/***/ "./components/Layout.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/_react@16.4.2@react/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Header__ = __webpack_require__("./components/Header.js");
+var _jsxFileName = "D:\\test\\hello-next\\components\\Layout.js";
+
+
+var layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+var Layout = function Layout(props) {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+    style: layoutStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    }
+  }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1__Header__["a" /* default */], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    }
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (Layout);
+
+/***/ })
+
+})
+//# sourceMappingURL=4.f36064d8771cf8d0b06b.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const Index = () => {
+  return (
+    <Layout>
+      <p>hello next.js</p>
+    </Layout> 
+  )
+}
+
+export default Index
